1cf117fec53df0fcdbf3e0341071672d712ac2cd
鷹取敏志 <takatorix@gmail.com>
2019-12-24 23:30:29 +0900

    refactor 0015

--- algorithms/0015_3sum.go
+++ algorithms/0015_3sum.go
@@ -1,23 +1,32 @@
 func threeSum(nums []int) [][]int {
+    var results [][]int    
+    sort.Ints(nums)
     
-    memo := make(map[string][]int, 0)
-    
-    for i := 0; i < len(nums); i++ {
-        for j := i + 1; j < len(nums); j++ {
-            for k := j + 1; k < len(nums); k++ {
-                if nums[i] + nums[j] + nums[k] == 0 {                                      
-                    l := []int{nums[i], nums[j], nums[k]}
-                    sort.Ints(l)
-                    memo[fmt.Sprint(l)] = l
+    for i := 0; i < len(nums) - 2; i++ {
+        if i > 0 && nums[i] == nums[i-1] {
+            continue
+        }
+        
+        target, left, right := -nums[i], i+1, len(nums)-1
+        
+        for left < right {
+            sum := nums[left] + nums[right]
+            if target == sum {
+                results = append(results, []int{nums[i], nums[left], nums[right]})
+                left++
+                right--
+                for left < right && nums[left] == nums[left-1] {
+                    left++
                 }
+                for left < right && nums[right] == nums[right+1] {
+                    right--
+                }
+            } else if sum < target {
+                left++
+            } else if sum > target {
+                right--
             }
         }
     }
-    
-    var result [][]int
-    for _, v := range memo {
-        result = append(result, v)
-    }
-    return result
+    return results
 }
-
