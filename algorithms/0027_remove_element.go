12a2f83c98c5c9795412a44d6a10c4f989b9c3a3
鷹取敏志 <takatorix@gmail.com>
2020-01-12 11:25:46 +0900

    fix 0027

--- algorithms/0027_remove_element.go
+++ algorithms/0027_remove_element.go
@@ -1,17 +1,13 @@
 func removeElement(nums []int, val int) int {
     var i int
-    for i < len(nums) { // i = 3, len = 5, [0,1,2,3,4]
-        j := i // j = 3
-        for j < len(nums) - 1 && nums[j] == nums[j+1] { 
-            j++ // j = 3
+    n := len(nums)    
+    for i < n {
+        if nums[i] == val {
+            nums[i] = nums[n-1]
+            n--
+        } else {
+            i++
         }
-        nums = append(nums[:i], nums[j:]...) // [0,1,2], [3,4] => [0,1,2,3,4]
-        i = i + 1 // i = 4
     }
-    return len(nums)
+    return n
 }
-
-
-// [0,0,1,2,2,2,3,4]
-// [0,1,2,2,2,3,4]
-// [0,1,2,3,4]
