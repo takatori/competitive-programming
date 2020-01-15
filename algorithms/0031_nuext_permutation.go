298f1d75693e742810c5024cbc254608fe351ece
鷹取敏志 <takatorix@gmail.com>
2020-01-15 23:36:00 +0900

    add 0031

---
+++ algorithms/0031_nuext_permutation.go
@@ -0,0 +1,30 @@
+func nextPermutation(nums []int)  {
+    i := len(nums) - 2
+    for i >= 0 && nums[i+1] <= nums[i] {
+        i--
+    }
+    if i >= 0 {
+        j := len(nums) -1
+        for j >= 0 && nums[j] <= nums[i] {
+            j--
+        }
+        swap(nums, i, j)
+    }
+    reverse(nums, i+1)
+}
+
+func reverse(nums []int, start int) {
+    i := start
+    j := len(nums) - 1
+    for i < j {
+        swap(nums, i, j)
+        i++
+        j--
+    }
+}
+
+func swap(nums[]int, i,j int) {
+    tmp := nums[i]
+    nums[i] = nums[j]
+    nums[j] = tmp
+}
