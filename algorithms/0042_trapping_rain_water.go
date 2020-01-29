d844434e6e97187cae013302ff54afe5f3e82b3d
鷹取敏志 <takatorix@gmail.com>
2020-01-29 23:30:00 +0900

    fix 0042

--- algorithms/0042_trapping_rain_water.go
+++ algorithms/0042_trapping_rain_water.go
@@ -1,21 +1,26 @@
 func trap(height []int) int { // [4,2,3]
    
-    var result int
-    var i int
+    if len(height) == 0 {
+        return 0
+    }
     
-    for i < len(height) { // i = 1,  len(height) = 3
-        j := i+1 // j = 2
-        w := 0
-        for j < len(height) && height[i] > height[j] { // 2 > 3
-            w += height[i] - height[j] // w = 2 + 1
-            j++ // j = 3
-        }
-        if j < len(height) {
-            result += w // 1 + 4 + 1
-            i = j // i = 10
-        } else {
-            i++
-        }
+    var result int
+    size := len(height)
+    left_max := make([]int, size)
+    right_max := make([]int, size)
+    
+    left_max[0] = height[0]
+    for i := 1; i < size; i++ {
+        left_max[i] = int(math.Max(float64(height[i]), float64(left_max[i-1])))
+    }
+    
+    right_max[size-1] = height[size-1]
+    for i := size-2; i >= 0; i-- {
+        right_max[i] = int(math.Max(float64(height[i]), float64(right_max[i+1])))
+    }
+    
+    for i := 1; i < size-1; i++ {        
+        result += int(math.Min(float64(left_max[i]), float64(right_max[i]))) - height[i]
     }
     
     return result
