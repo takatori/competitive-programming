b3cc6032a8fc379bc164dce721f508cf383d843e
鷹取敏志 <takatorix@gmail.com>
2020-01-31 23:05:48 +0900

    fix 0043

--- algorithms/0043_multiply_strings.go
+++ algorithms/0043_multiply_strings.go
@@ -7,23 +7,22 @@ func multiply(num1 string, num2 string) string {
     
     for i := m-1; i >= 0; i-- {
         for j := n-1; j >= 0; j-- {
-            mul := int((num1[i] - '0') * (num2[j] - '0'))
-            p1 := i+j
-            p2 := i+j+1
-            sum := mul + pos[p2]
-            
-            pos[p1] += sum / 10
-            pos[p2] = (sum) % 10
+            mul := int((num1[i] - '0') * (num2[j] - '0')) 
+            p1 := i+j 
+            p2 := i+j+1 
+            sum := mul + pos[p2] 
+            pos[p1] += sum / 10 
+            pos[p2] = sum % 10  
         }
     }
     
-    var result int
+    result := "0"
     
     for _, p := range pos {
-        if !(result == 0 && p == 0) {
-            result += p
+        if !(result == "0" && p == 0) {
+            result += strconv.Itoa(p)
         }
     }
     
-    return strconv.Itoa(result)
+    return result
 }
