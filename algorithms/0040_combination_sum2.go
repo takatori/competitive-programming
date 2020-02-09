e2d063220dd6943e110e4dc3a952a1ea8f8302ac
鷹取敏志 <takatorix@gmail.com>
2020-02-09 15:22:48 +0900

    fix 0040

--- algorithms/0040_combination_sum2.go
+++ algorithms/0040_combination_sum2.go
@@ -11,12 +11,16 @@ func backtrace(cur []int, candidates []int, target int) {
     if target == 0 {
         result = append(result, cur)
     } else {
-        for i, v := range candidates {
+        for i := 0; i < len(candidates); i++ {
+            v := candidates[i]
             if v <= target {
                 tmp := make([]int, 0, len(cur))
                 tmp = append(tmp, cur...)
                 tmp = append(tmp, v)
                 backtrace(tmp, candidates[i+1:], target-v)
+                for i < len(candidates)-1 && v == candidates[i+1] {
+                    i++
+                }
             } else {
                 break
             }
