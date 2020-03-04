aebd2036332a8db3685e2446506787e65f4a75d8
鷹取敏志 <takatorix@gmail.com>
2020-03-04 23:09:51 +0900

    fix 0062

--- algorithms/0062_unique_paths.go
+++ algorithms/0062_unique_paths.go
@@ -1,6 +1,10 @@
 func uniquePaths(m int, n int) int {
-    if m == 1 || n == 1 {
-        return 1
+    dp := make([]int, m)
+    dp[0] = 1
+    for i := 0; i < n; i++ {
+        for j := 1; j < len(dp); j++ {
+            dp[j] += dp[j-1]
+        }
     }
-    return uniquePaths(m, n-1) + uniquePaths(m-1, n)
+    return dp[len(dp)-1]    
 }
