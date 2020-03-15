d24df96756753a032aa13b1add1b8d557c160c78
鷹取敏志 <takatorix@gmail.com>
2020-03-15 23:03:53 +0900

    fix 0071

--- algorithms/0071_simplify_path.go
+++ algorithms/0071_simplify_path.go
@@ -1,23 +1,17 @@
-
-
 func simplifyPath(path string) string {
  
-    result := []rune{}
-    var prev rune
-    for i, c := range path {
-        if c == '.' && prev == '.' {
-            result = result[:len(result)-2]         
-            continue
-        } 
-        if c != '.' && c != '/' {
-            result = append(result, c)
-        }
-        if c == '/' && prev != '/' && i != len(path)-1 {
-            result = append(result, '/')
-        }
-        prev = c
-    }
-    
-    return string(result)
+	stack := make([]string, 0)
+	parts := strings.Split(path, "/")
+	for _, part := range parts {
+		if part == "" || part == "." {
+			continue
+		} else if part == ".." {
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+		} else {
+			stack = append(stack, part)
+		}
+	}
+	return "/" + strings.Join(stack, "/")
 }
-
