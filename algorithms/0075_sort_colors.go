b13f4178882ea242e12f20d86f5909d8b1ad4434
鷹取敏志 <takatorix@gmail.com>
2020-03-19 23:17:14 +0900

    fix 0075

--- algorithms/0075_sort_colors.go
+++ algorithms/0075_sort_colors.go
@@ -1,3 +1,17 @@
 func sortColors(nums []int)  {
-	sort.Ints(nums)
+    
+    red, white, blue := 0, 0, len(nums)-1
+
+    for white <= blue {
+        if nums[white] == 0 {
+            nums[red], nums[white] = nums[white], nums[red]
+            red++
+            white++
+        } else if nums[white] == 1 {
+            white++
+        } else {
+            nums[white], nums[blue] = nums[blue], nums[white]
+            blue--
+        }
+    }
 }
