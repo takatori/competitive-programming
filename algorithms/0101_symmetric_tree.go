47d3f9109c8c5c04c2ea83d1d5bb6fb8c516add7
鷹取敏志 <takatorix@gmail.com>
2020-03-30 21:41:10 +0900

    add 0101

--- algorithms/0101_symmetric_tree.go
+++ algorithms/0101_symmetric_tree.go
@@ -8,35 +8,24 @@
  */
 func isSymmetric(root *TreeNode) bool {
     
-    if root == nil || (root.Left == nil && root.Right == nil) {
-        return true
-    } else if root.Left == nil || root.Right == nil {
-        return false
-    }
+    q := make([]*TreeNode, 0)
+    q = append(q, root, root)
     
-    queue := make([]*TreeNode, 0)
-    
-    for len(queue) > 0 {
-        
-        nextQueue := make([]*TreeNode, 0)
-        l := 0
-        r := len(queue)-1
-        
-        for l < r {
-            if (queue[l].Value != queue[r].Value) || (queue[l].Left == nil && queue[r].Right != nil) || {
-                return false
-            }
-            if queue[l].Right != nil {
-                nextQueue = append([]*Tree{queue[l].Right}, append(nextQueue, queue[r].Left)...)
-            }
-            if queue[l].Left != nil {
-                 nextQueue = append([]*Tree{queue[l].Left}, append(nextQueue, queue[r].Right)...)
-            }
-            l++
-            r--
+    for len(q) > 0 {
+        t1 := q[0]
+        t2 := q[1]
+        q = q[2:]
+        if t1 == nil && t2 == nil {
+            continue
         }
+        if t1 == nil || t2 == nil {
+            return false
+        }
+        if t1.Val != t2.Val {
+            return false
+        }
+        q = append(q, t1.Left, t2.Right, t1.Right, t2.Left)
     }
 
     return true
 }
-
