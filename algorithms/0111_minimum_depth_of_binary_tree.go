1707ffd385084bf2b81b7491ed194db858d18168
鷹取敏志 <takatorix@gmail.com>
2020-05-25 23:12:17 +0900

    add 0111

---
+++ algorithms/0111_minimum_depth_of_binary_tree.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func minDepth(root *TreeNode) int {
+    
+	if root == nil {
+	  return 0
+	}
+	
+	if root.Left == nil && root.Right == nil {
+	  return 1
+	}
+   
+	if root.Left == nil {
+	  return minDepth(root.Right)+1
+	}
+	
+	if root.Right == nil {
+	  return minDepth(root.Left)+1
+	}
+	
+	lh := minDepth(root.Left)
+	rh := minDepth(root.Right)
+	
+	if lh > rh {
+	  return rh+1
+	} else {
+	  return lh+1
+	}  
+  }
