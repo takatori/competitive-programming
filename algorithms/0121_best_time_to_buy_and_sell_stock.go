66daacf5396c2351593afc1e284ad48cf0faa564
鷹取敏志 <takatorix@gmail.com>
2020-07-28 23:32:10 +0900

    refactor 0121

--- algorithms/0121_best_time_to_buy_and_sell_stock.go
+++ algorithms/0121_best_time_to_buy_and_sell_stock.go
@@ -1,24 +1,16 @@
-
 func maxProfit(prices []int) int {
-    
-	if len(prices) == 0 {
-	  return 0
-	}
-	
-	var max int
   
-	for i := 0; i < len(prices)-1; i++ {
-	  for j := i+1; j < len(prices); j++ {
-		diff := prices[j] - prices[i]
-		if diff > max {
-		  max = diff
-		}
+	minprice := math.MaxInt64
+	maxprofit := 0
+	
+	for i := 0; i < len(prices); i++ {
+	  if prices[i] < minprice {
+		minprice = prices[i]
+	  } else if (prices[i] - minprice) > maxprofit {
+		maxprofit = prices[i] - minprice
 	  }
 	}
 	
-	return max
-	  
+	return maxprofit
   }
-  
-  
   
