d76c51cd5e662c428408a672838ea586f0b994db
鷹取敏志 <takatorix@gmail.com>
2021-01-21 00:15:07 +0900

    Fix: 0146

--- algorithms/0146_lru_cache.go
+++ algorithms/0146_lru_cache.go
@@ -1,61 +1,84 @@
-type CacheItem struct {
-	value int
-	count int
-  }
-  
-  type LRUCache struct {
-	capacity int
-	cache map[int]*CacheItem
-	usage map[int]map[int]*CacheItem
-  }
-  
-  
-  func Constructor(capacity int) LRUCache {
-	  
-  }
-  
-  
-  func (this *LRUCache) Get(key int) int {
-	
-	if item, ok := this.cache[key]; !ok {
-	  return -1
-	} else {
-	  v := item.value
-	  c := item.count
-	  
-	  // 回数のクラスに存在するか
-	  if i, ok := usage[c]; ok {
-		// 既に存在していたら消す
-		if _, ok := i[v]; ok {
-		  delete(i, v)
-		}
-	  }
-	  
-	  item.count++
-	  c++
-	  
-	  // 追加
-	  if i, ok := usage[c]; !ok {
-		m := map[int]*CacheItem{}
-		m[v] = item
-		usage[c] = m
-	  } else {
-		
-	  }
-  
-  
-	  return item.Value
+type LRUCache struct {
+	head *node
+	tail *node
+	store map[int]*node
+	cap int
+}
+
+type node struct {
+	prev *node
+	next *node
+	key int
+	val int
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		cap: capacity,
+		store: make(map[int]*node),
 	}
-  }
-  
-  
-  func (this *LRUCache) Put(key int, value int)  {
-	  
-  }
-  
-  /**
-   * Your LRUCache object will be instantiated and called as such:
-   * obj := Constructor(capacity);
-   * param_1 := obj.Get(key);
-   * obj.Put(key,value);
-   */
+}
+
+func (c *LRUCache) removeFromChain(n *node) {
+	if n == c.head {
+		c.head = n.next
+	}
+
+	if n == c.tail {
+		c.tail = n.prev
+	}
+
+	if n.next != nil {
+		n.next.prev = n.prev
+	}
+
+	if n.prev != nil {
+		n.prev.next = n.next
+	}
+}
+
+func (c *LRUCache) addToChain(n *node) {
+	n.prev = nil
+	n.next = c.head
+	if n.next != nil {
+		n.next.prev = n
+	}
+	c.head = n
+	if c.tail == nil {
+		c.tail = n
+	}
+}
+
+func (c *LRUCache) Get(key int) int {
+	n, ok := c.store[key]
+	if !ok {
+		return -1
+	}
+	c.removeFromChain(n)
+	c.addToChain(n)
+	return n.val
+}
+
+func (c *LRUCache) Put(key int, value int) {
+	n, ok := c.store[key]
+	if !ok {
+		n = &node{
+			key: key,
+			val: value,
+		}
+		c.store[key] = n
+	} else {
+		n.val = value
+		c.removeFromChain(n)
+	}
+
+	c.addToChain(n)
+
+	if len(c.store)  > c.cap {
+		n = c.tail
+		if n != nil {
+			c.removeFromChain(n)
+			delete(c.store, n.key)
+		}
+	}
+}
