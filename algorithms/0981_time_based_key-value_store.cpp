72c7c7341ba231948d1150b1a4c7a9240c5cd3a0
鷹取敏志 <takatorix@gmail.com>
2021-03-16 22:40:10 +0900

    fix 0981

--- algorithms/0981_time_based_key-value_store.cpp
+++ algorithms/0981_time_based_key-value_store.cpp
@@ -11,56 +11,14 @@ public:
 
     void set(string key, string value, int timestamp)
     {
-        auto newItem = make_pair(timestamp, value);
-        if (this->store.count(key) == 0)
-        {
-            this->store[key] = vector<pair<int, string>>{newItem};
-        }
-        else
-        {
-            this->store[key].push_back(newItem);
-        }
+        this->store[key].push_back(make_pair(timestamp, value));
     }
 
     string get(string key, int timestamp)
     {
-        if (this->store.count(key) == 0)
-        {
-            return "";
-        }
-        auto items = this->store[key];
-        int idx = upper_bound(items, timestamp);
-        if (idx == -1)
-        {
-            return "";
-        }
-        return items.at(idx - 1).second;
+        auto it = upper_bound(begin(this->store[key]), end(this->store[key]), pair<int, string>(timestamp, ""), [](
+            const pair<int, string>& a, const pair<int, string>& b) { return a.first < b.first; });
+        return it == this->store[key].begin() ? "" : prev(it)->second;
     }
 
-    int upper_bound(vector<pair<int, string>> arr, int timestamp)
-    {
-        int mid;
-        int left = 0;
-        int right = arr.size();
-
-        while (left < right)
-        {
-            mid = (right - left) / 2;
-            if (timestamp >= arr.at(mid).first)
-            {
-                left = mid + 1;
-            }
-            else
-            {
-                right = mid;
-            }
-        }
-
-        if (right == 0)
-        {
-            return -1;
-        }
-
-        return left;
-    }
 };
