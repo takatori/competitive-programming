251c59945a192cbfe8c29c14ac64feeada60bb53
鷹取敏志 <takatorix@gmail.com>
2021-03-19 23:58:36 +0900

    fix 0295

--- algorithms/0295_find_median_from_data_stream.cpp
+++ algorithms/0295_find_median_from_data_stream.cpp
@@ -1,12 +1,10 @@
-#include <list>
-
 using namespace std;
 
 class MedianFinder
 {
 private:
     list<int> li;
-    list<int>::iterator it = li.begin();
+    list<int>::iterator it = this->li.end();
 
 public:
     /** initialize your data structure here. */
@@ -16,26 +14,21 @@ public:
 
     void addNum(int num)
     {
+        const int n = this->li.size();
         this->li.insert(lower_bound(this->li.begin(), this->li.end(), num), num);
-        if (*this->it < num && next(this->it) != this->li.end())
-        {
-            this->it = next(this->it);
-        }
-        else if (*this->it >= num && prev(this->it) != this->li.begin())
-        {
-            this->it = prev(this->it);
-        }
+        if (!n)
+            this->it = this->li.begin();
+        else if (num < *this->it)
+            this->it = n & 1 ? this->it : prev(this->it);
+        else
+            this->it = n & 1 ? next(this->it) : this->it;
     }
 
     double findMedian()
     {
-        if (this->li.size() % 2 == 0)
-        {
-            return double(*this->it + *next(this->it)) / 2;
-        }
-        else
-        {
-            return double(*this->it);
-        }
+        const int n = this->li.size();
+        double a = (double)*this->it;
+        double b = (double)*next(this->it, n % 2 - 1);
+        return (a + b) * 0.5;
     }
 };
